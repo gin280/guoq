b3200f37d6cd5a88721d990de6330021d2712297
gin280 <zxy_wtfm@163.com>
2024-01-13 19:14:05 +0800

    feat: welcome

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-console.log("test 1")
+const pkgJSON = require("./package.json")
+const welcome = require("cli-welcome")
+
+welcome({
+  title: pkgJSON.name,
+  tagLine: "by " + pkgJSON.author.name,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: "#FADC00",
+  color: "#000000",
+  bold: true,
+  clear: true,
+})
+
+console.log(`
+  NAME: ${pkgJSON.name}
+  VERSION: v${pkgJSON.version}
+  DESCRIPTION: ${pkgJSON.description}
+
+  Usage:
+    $ npx ${pkgJSON.name} [options]
+
+
+`)

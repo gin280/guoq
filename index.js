e361aa8e35df42c2defab02cbe61378fd20bee17
gin280 <zxy_wtfm@163.com>
2024-01-13 19:57:19 +0800

    change code

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 #!/usr/bin/env node
 
-const pkgJSON = require("./package.json")
-const welcome = require("cli-welcome")
+import pkgJSON from "./package.json" assert { type: "json" }
+import welcome from "cli-welcome"
+import chalk from "chalk"
+
+const log = console.log
 
 welcome({
-  title: pkgJSON.name,
+  title: `Guoq`,
   tagLine: "by " + pkgJSON.author.name,
   description: pkgJSON.description,
   version: pkgJSON.version,
-  bgColor: "#FADC00",
+  bgColor: "#6937ff",
   color: "#000000",
   bold: true,
   clear: true,
 })
 
-console.log(`
-  NAME: ${pkgJSON.name}
-  VERSION: v${pkgJSON.version}
-  DESCRIPTION: ${pkgJSON.description}
-
-  Usage:
-    $ npx ${pkgJSON.name} [options]
-
+log(`
+   
+  ${chalk.italic(`A simple CLI to generate a GraphQL schema from a JSON file.
+  A simple CLI to generate a GraphQL schema from a JSON file.
+  A simple CLI to generate a GraphQL schema from a JSON file.`)}
+  
+  ${chalk.hex(`#1da1f2`).bold.inverse("WEBSITE")} 'http://www.gq.com'
+  ${chalk.hex(`#6cc644`).bold.inverse("GITHUB")}  'https:www.github.com/gq'
+  ${chalk.hex(`#ff0000`).bold.inverse("NPM")} 'https:www.npm.com/gq'
 
 `)
+
+log(
+  chalk.greenBright.underline("Success!") +
+    " Created a GraphQL schema from a JSON file."
+)
